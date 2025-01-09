041b1ef9d153e270837883f5cf549f6594bcfee8
BBT221060 <bravebajwa12@gmail.com>
2025-01-09 23:41:23 +0500

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, original, reversed = 0;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    original = number; 
+
+    
+    while (number != 0) {
+        int digit = number % 10;    
+        reversed = reversed * 10 + digit; 
+        number /= 10;             
+    }
+
+    if (original == reversed) {
+        cout << original << " is a palindrome." << endl;
+    } else {
+        cout << original << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
